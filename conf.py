1827335218cacdca1eaac9418ee41148d26ac99d
ZGGSONG <zggsong@foxmail.com>
2020-07-14 14:02:29 +0800

    Update site update

--- conf.py
+++ conf.py
@@ -21,7 +21,7 @@ enable_jsdelivr = {
 
 # 站点设置
 # For site
-site_name = "艾芝士"
+site_name = "啃芝士"
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2020-07-14T00:00+08:00"
 author = "ZGGSONG"
